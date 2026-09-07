66ba5b6e4b7e27af5fc708970707cd164d7c645e
br1an17 <brianzairpaez@gmail.com>
2026-09-07 17:15:42 -0300

    cambios en Product list a la vercion anterior

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,10 +3,10 @@ import ProductCard from "./ProductCard";
 
 const ProductList = ({ products, onSelect }) => {
   return (
-    <Container sx={{ py: 2 }}>
+    <Container maxWidth="lg" sx={{ py: 4 }}>
       <Grid container spacing={3}>
         {products.map((product) => (
-          <Grid key={product.id} size={{ xs: 12, sm: 6, md: 4 }}>
+          <Grid item key={product.id} xs={12} sm={6} md={4}>
             <ProductCard product={product} onSelect={onSelect} />
           </Grid>
         ))}
